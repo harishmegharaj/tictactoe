82c3cf87185307f97369d4efbfdd326f9fd4f6ff
Harish Ashok Megharaj <harish.megharaj@gmail.com>
2020-10-15 20:44:08 -0700

    Code Realigned

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -5,12 +5,10 @@ const HomePage = () => {
 	const [gameId, setGameId] = React.useState(1);
 
 	return (
-		<>
-			<PlayGame
-				key={gameId}
-				startNewGame={() => setGameId(gameId + 1)} />
+		<PlayGame
+			key={gameId}
+			startNewGame={() => setGameId(gameId + 1)} />
 
-		</>
 	)
 }
 
